01db2cf593f4018de535d1d2efcde2047f8c26b4
Lautaro Rodriguez <lautarodriguez17@gmail.com>
2019-09-23 16:40:49 -0300

    deleted folder components

--- src/App.js
+++ src/App.js
@@ -2,14 +2,17 @@ import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-import Navigation from './components/Navigation';
-
 class App extends Component {
   render () { 
     return (
       <div className="App">
-        <Navigation titulo='Mi primer titulo'/>
-        <Navigation titulo='Mi segundo titulo'/>
+
+      <nav className='navbar navbar-dark bg-dark'>
+                <a href='app.js' className='text-white'>
+                  Tasks
+                </a>
+          </nav>
+
           <img src={logo} className="App-logo" alt="logo" />
       </div>
     );
