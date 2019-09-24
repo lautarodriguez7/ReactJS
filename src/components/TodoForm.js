fdc7727e48e73259697a103f80d2b428593b14f9
Lautaro Rodriguez <lautarodriguez17@gmail.com>
2019-09-24 20:00:19 -0300

    adding code

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,5 +1,11 @@
 import React, { Component } from 'react';
 
 class TodoForm extends Component {
-
-}
+  constructor () {
+    super();
+    this.state = {
+      title: '',
+      responsible: '',
+      description: '',
+      priority: 'low'
+    };
